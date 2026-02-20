47384720817f250af11edda5e5f976a8fafdb745
youssefgamal285 <youssefgamal4150@gmail.com>
2026-02-20 17:31:59 +0200

    .

---
+++ Frontend/HomePage/script.js
@@ -0,0 +1,13 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (email === "" || password === "") {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    alert("Login successful");
+});
